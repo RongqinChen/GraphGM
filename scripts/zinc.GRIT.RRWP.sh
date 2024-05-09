ec166306ce157537f070ca596dd4ba500fd0fcfd
RongqinChen <yc07917@umac.mo>
2024-05-09 14:43:43 +0800

    update

--- scripts/zinc.GRIT.RRWP.sh
+++ scripts/zinc.GRIT.RRWP.sh
@@ -1,6 +1,6 @@
 
-python main.py --cfg configs/GRIT/zinc-GRIT-RRWP.yaml  wandb.use False accelerator "cuda:0" seed 0  &
-python main.py --cfg configs/GRIT/zinc-GRIT-RRWP.yaml  wandb.use False accelerator "cuda:0" seed 1  &
-python main.py --cfg configs/GRIT/zinc-GRIT-RRWP.yaml  wandb.use False accelerator "cuda:0" seed 2  &
-python main.py --cfg configs/GRIT/zinc-GRIT-RRWP.yaml  wandb.use False accelerator "cuda:0" seed 3  &
+CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/zinc-GRIT-RRWP.yaml wandb.use False seed 0  &
+CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/zinc-GRIT-RRWP.yaml wandb.use False seed 1  &
+CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/zinc-GRIT-RRWP.yaml wandb.use False seed 2  &
+CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/zinc-GRIT-RRWP.yaml wandb.use False seed 3  &
 wait
