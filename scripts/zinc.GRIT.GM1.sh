bba752ddd57c6fe9d7c5a3404eef02f4de74ad66
RongqinChen <yc07917@umac.mo>
2024-05-10 20:23:12 +0800

    update

--- scripts/zinc.GRIT.GM1.sh
+++ scripts/zinc.GRIT.GM1.sh
@@ -1,13 +1,16 @@
 CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/zinc-GRIT-GM1.yaml wandb.use False seed 0  &
-# wait
+wait
+
 CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/zinc-GRIT-GM1.yaml wandb.use False seed 1  &
 wait
 
 CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/zinc-GRIT-GM1.yaml wandb.use False seed 2  &
-# wait
-CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/zinc-GRIT-GM1.yaml wandb.use False seed 3  &
-
-# wait
-# CUDA_VISIBLE_DEVICES=1 python main.py --cfg configs/GRIT/zinc-GRIT-GM1.yaml wandb.use False seed 4  &
-
 wait
+
+CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/zinc-GRIT-GM1.yaml wandb.use False seed 3  &
+wait
+
+# CUDA_VISIBLE_DEVICES=1 python main.py --cfg configs/GRIT/zinc-GRIT-GM1.yaml wandb.use False seed 4  &
+# wait
+
+python -m graphgps.agg_runs 
