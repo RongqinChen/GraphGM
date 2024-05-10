8dd88b5f04127f47f1bca648ee96ee93f5ce0a7f
RongqinChen <yc07917@umac.mo>
2024-05-11 03:30:23 +0800

    update

--- scripts/zincfull.GRIT.GM1.sh
+++ scripts/zincfull.GRIT.GM1.sh
@@ -1,11 +1,11 @@
 CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/zincfull-GRIT-GM1.yaml wandb.use False seed 0 name_tag T102038  &
-wait
+# wait
 
 CUDA_VISIBLE_DEVICES=1 python main.py --cfg configs/GRIT/zincfull-GRIT-GM1.yaml wandb.use False seed 1 name_tag T102038  &
-wait
+# wait
 
 CUDA_VISIBLE_DEVICES=2 python main.py --cfg configs/GRIT/zincfull-GRIT-GM1.yaml wandb.use False seed 2 name_tag T102038  &
-wait
+# wait
 
 CUDA_VISIBLE_DEVICES=3 python main.py --cfg configs/GRIT/zincfull-GRIT-GM1.yaml wandb.use False seed 3 name_tag T102038  &
 wait
