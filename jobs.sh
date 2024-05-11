ecd8fc37845e049a9d248d5a2f43db4e2ad801fa
RongqinChen <yc07917@umac.mo>
2024-05-11 12:41:12 +0800

    update

--- jobs.sh
+++ jobs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name              func_grit_bern
+#SBATCH --job-name              pat_grit_bern
 #SBATCH --time                  48:00:00
 #SBATCH --cpus-per-task         6
 #SBATCH --gres                  gpu:1
 #SBATCH --mem                   200G
-#SBATCH --output=results/func_grit_bern.out
-#SBATCH --partition             a100_batch
+#SBATCH --output=results/pat_grit_bern.out
+#SBATCH --partition             h800_batch
 
 your_cleanup_function()
 {
@@ -28,10 +28,28 @@ echo ""
 nvidia-smi
 echo ""
 
-/home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-func-GRIT-Bern.yaml wandb.use False seed 0 
-/home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-func-GRIT-Bern.yaml wandb.use False seed 1 
-/home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-func-GRIT-Bern.yaml wandb.use False seed 2 
-/home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-func-GRIT-Bern.yaml wandb.use False seed 3 
-/home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-func-GRIT-Bern.yaml wandb.use False seed 4 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-func-GRIT-Bern.yaml wandb.use False seed 0 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-func-GRIT-Bern.yaml wandb.use False seed 1 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-func-GRIT-Bern.yaml wandb.use False seed 2 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-func-GRIT-Bern.yaml wandb.use False seed 3 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-func-GRIT-Bern.yaml wandb.use False seed 4 
+
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-struct-GRIT-Bern.yaml wandb.use False seed 0 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-struct-GRIT-Bern.yaml wandb.use False seed 1 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-struct-GRIT-Bern.yaml wandb.use False seed 2 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-struct-GRIT-Bern.yaml wandb.use False seed 3 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/peptides-struct-GRIT-Bern.yaml wandb.use False seed 4 
+
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/cluster-GRIT-Bern.yaml wandb.use False seed 0 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/cluster-GRIT-Bern.yaml wandb.use False seed 1 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/cluster-GRIT-Bern.yaml wandb.use False seed 2 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/cluster-GRIT-Bern.yaml wandb.use False seed 3 
+# /home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/cluster-GRIT-Bern.yaml wandb.use False seed 4 
+
+/home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/pattern-GRIT-Bern.yaml wandb.use False seed 0 
+/home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/pattern-GRIT-Bern.yaml wandb.use False seed 1 
+/home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/pattern-GRIT-Bern.yaml wandb.use False seed 2 
+/home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/pattern-GRIT-Bern.yaml wandb.use False seed 3 
+/home/yc07917/mambaforge/envs/gnn210/bin/python main.py --cfg configs/GRIT/pattern-GRIT-Bern.yaml wandb.use False seed 4 
 
 echo "finished!!!"
