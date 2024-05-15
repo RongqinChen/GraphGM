261381185cd70477359dd5a4282497aa647b46e8
RongqinChen <yc07917@umac.mo>
2024-05-15 20:09:22 +0800

    update

--- tasks.sh
+++ tasks.sh
@@ -1,12 +1,12 @@
-# K=14
-# CUDA_VISIBLE_DEVICES=0 \
-#     python main.py --repeat 5 --cfg configs/GSE/zinc/GRIT-RRW_Bern.yaml \
-#     posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2)*(K+1)/2 )) name_tag K$((K)) &
-# wait
+K=14
+CUDA_VISIBLE_DEVICES=0 \
+    python main.py --repeat 5 --cfg configs/GSE/zinc/GRIT-RRW_Bern.yaml \
+    posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2)*(K+1)/2 )) name_tag K$((K)) &
+wait
 
 
 K=14
 CUDA_VISIBLE_DEVICES=0  \
     python main.py --repeat 5 --cfg configs/GSE/pattern/pattern-RRW_Bern.yaml \
-    posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2)*(K+1)/2 )) name_tag K$((K)) print stdout &
+    posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2)*(K+1)/2 )) name_tag K$((K)) &
 wait
