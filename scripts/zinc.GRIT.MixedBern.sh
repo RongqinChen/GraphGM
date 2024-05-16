1bf5b33f1d200a1011605d6ca529c896694648c5
RongqinChen <yc07917@umac.mo>
2024-05-16 12:47:56 +0800

    update

---
+++ scripts/zinc.GRIT.MixedBern.sh
@@ -0,0 +1,34 @@
+
+K=16
+CUDA_VISIBLE_DEVICES=0 \
+    python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GT-GRIT-Poly.yaml  \
+    posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
+    name_tag mixed_bern_K$((K)) &
+
+wait
+
+K=18
+CUDA_VISIBLE_DEVICES=0 \
+    python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GT-GRIT-Poly.yaml  \
+    posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
+    name_tag mixed_bern_K$((K)) &
+
+wait
+
+
+K=20
+CUDA_VISIBLE_DEVICES=0 \
+    python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GT-GRIT-Poly.yaml  \
+    posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
+    name_tag mixed_bern_K$((K)) &
+
+wait
+
+
+K=22
+CUDA_VISIBLE_DEVICES=0 \
+    python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GT-GRIT-Poly.yaml  \
+    posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
+    name_tag mixed_bern_K$((K)) &
+
+wait
