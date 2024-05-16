1eb81890b211f5d375f3a4e6101e5608b32a7152
RongqinChen <yc07917@umac.mo>
2024-05-16 21:56:12 +0800

    update

--- scripts/pcqm4m-contact.GRIT.RRWP.sh
+++ scripts/pcqm4m-contact.GRIT.RRWP.sh
@@ -1,17 +1,17 @@
 
-CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/pcqm-contact-GRIT-RRWP.yaml wandb.use False seed 0  &
+CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/pcqm_contact-GRIT-RRWP.yaml wandb.use False seed 0 print stdout &
 wait
 
-CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/pcqm-contact-GRIT-RRWP.yaml wandb.use False seed 1  &
+CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/pcqm_contact-GRIT-RRWP.yaml wandb.use False seed 1 print stdout &
 wait
 
-CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/pcqm-contact-GRIT-RRWP.yaml wandb.use False seed 2  &
+CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/pcqm_contact-GRIT-RRWP.yaml wandb.use False seed 2 print stdout &
 wait
 
-CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/pcqm-contact-GRIT-RRWP.yaml wandb.use False seed 3  &
+CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/pcqm_contact-GRIT-RRWP.yaml wandb.use False seed 3 print stdout &
 wait
 
-CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/pcqm-contact-GRIT-RRWP.yaml wandb.use False seed 4  &
+CUDA_VISIBLE_DEVICES=0 python main.py --cfg configs/GRIT/pcqm_contact-GRIT-RRWP.yaml wandb.use False seed 4 print stdout &
 wait
 
 python -m graphgps.agg_runs 
