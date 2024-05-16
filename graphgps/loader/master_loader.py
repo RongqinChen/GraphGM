d8c64e7904fcdd7231eee56fb89c7a4c5d7e954f
RongqinChen <yc07917@umac.mo>
2024-05-17 01:03:15 +0800

    update

--- graphgps/loader/master_loader.py
+++ graphgps/loader/master_loader.py
@@ -189,7 +189,7 @@ def load_dataset_master(format, name, dataset_dir):
         if f"{format}_{name}" in {"PyG-ZINC_full"} or \
                 name in {"peptides-functional", "peptides-structural", "CLUSTER",
                          "PCQM4Mv2-full", "CIFAR10", "MNIST", "PCQM4Mv2Contact-shuffle",
-                         "edge_wt_region_boundary"}:
+                         "edge_wt_region_boundary", "ogbg-molpcba"}:
             logging.info(
                 f"Positional Encoding statistics: {pe_enabled_list} "
                 f"will be computed on the fly for all graphs..."
