f6f9e88d10606169fc97474a81e7745f1b0ee341
RongqinChen <yc07917@umac.mo>
2024-05-19 17:20:09 +0000

    update

--- scripts/zinc/zincfull-GSE-GRIT-Poly.sh
+++ scripts/zinc/zincfull-GSE-GRIT-Poly.sh
@@ -2,7 +2,7 @@
 K=4
 L=3
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GSE.yaml  \
+python main.py --repeat 5 --cfg configs/GSE/zinc/zincfull-GSE.yaml  \
 posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
 gse_model.messaging.num_blocks $((L)) \
 name_tag mixed_bern_K$((K))L$((L)) &
@@ -13,7 +13,7 @@ wait
 K=6
 L=3
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GSE.yaml  \
+python main.py --repeat 5 --cfg configs/GSE/zinc/zincfull-GSE.yaml  \
 posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
 gse_model.messaging.num_blocks $((L)) \
 name_tag mixed_bern_K$((K))L$((L)) &
@@ -24,7 +24,7 @@ wait
 K=8
 L=4
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GSE.yaml  \
+python main.py --repeat 5 --cfg configs/GSE/zinc/zincfull-GSE.yaml  \
 posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
 gse_model.messaging.num_blocks $((L)) \
 name_tag mixed_bern_K$((K))L$((L)) &
@@ -35,7 +35,7 @@ wait
 K=10
 L=4
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GSE.yaml  \
+python main.py --repeat 5 --cfg configs/GSE/zinc/zincfull-GSE.yaml  \
 posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
 gse_model.messaging.num_blocks $((L)) \
 name_tag mixed_bern_K$((K))L$((L)) &
