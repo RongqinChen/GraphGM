76eaeceac6b230f21b70ffcef0d83bed286afbf9
RongqinChen <yc07917@umac.mo>
2024-05-21 13:21:13 +0800

    update

--- tasks.sh
+++ tasks.sh
@@ -3,7 +3,20 @@ K=4
 L=3
 R=1
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_grit-Poly.yaml  \
+python main.py --cfg configs/GSE/peptides/peptides_func-GSE_grit-Poly.yaml  \
+seed 4 posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
+gse_model.messaging.num_blocks $((L)) \
+gse_model.full.repeats $((R)) \
+name_tag mixed_bern_K$((K))L$((L))R$((R)) &
+
+# wait
+
+
+K=6
+L=3
+R=1
+CUDA_VISIBLE_DEVICES=1 \
+python main.py --cfg configs/GSE/peptides/peptides_func-GSE_dense-Poly.yaml  \
 seed 4 posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
 gse_model.messaging.num_blocks $((L)) \
 gse_model.full.repeats $((R)) \
@@ -15,33 +28,20 @@ wait
 K=6
 L=3
 R=1
-CUDA_VISIBLE_DEVICES=0 \
-python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_dense-Poly.yaml  \
+CUDA_VISIBLE_DEVICES=1 \
+python main.py --cfg configs/GSE/peptides/peptides_func-GSE_grit-Poly.yaml  \
 seed 4 posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
 gse_model.messaging.num_blocks $((L)) \
 gse_model.full.repeats $((R)) \
 name_tag mixed_bern_K$((K))L$((L))R$((R)) &
 
-wait
-
-
-K=6
-L=3
-R=1
-CUDA_VISIBLE_DEVICES=0 \
-python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_grit-Poly.yaml  \
-seed 4 posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
-gse_model.messaging.num_blocks $((L)) \
-gse_model.full.repeats $((R)) \
-name_tag mixed_bern_K$((K))L$((L))R$((R)) &
-
-wait
+# wait
 
 K=4
 L=3
 R=1
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_dense-Poly.yaml  \
+python main.py --cfg configs/GSE/peptides/peptides_func-GSE_dense-Poly.yaml  \
 seed 4 posenc_Poly.method mixed_bern posenc_Poly.order $((K)) posenc_Poly.emb_dim $(( (K+2) ))  \
 gse_model.messaging.num_blocks $((L)) \
 gse_model.full.repeats $((R)) \
