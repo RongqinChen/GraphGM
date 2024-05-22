43a1cb7a92b5afc5271c3da2e42aac6cb2c79597
RongqinChen <yc07917@umac.mo>
2024-05-22 13:04:26 +0800

    update

--- graphgps/network/gse_model.py
+++ graphgps/network/gse_model.py
@@ -68,15 +68,15 @@ class GseModel(torch.nn.Module):
     def __init__(self, dim_in, dim_out):
         super().__init__()
         assert (cfg.posenc_Poly.emb_dim - 2) >= 2 ** (cfg.gse_model.messaging.num_blocks - 1)
-        self.feat_encoder = FeatureEncoder(cfg.gse_model.hidden_dim)
-
         GseMessagingBlock = register.layer_dict["GseMessagingBlock"]
         GseFullBlock = register.layer_dict["GseFullBlock"]
-        LoopEncoder = register.node_encoder_dict["sparse_poly"]
-        ConnEncoder = register.edge_encoder_dict["sparse_poly"]
+
+        self.feat_encoder = FeatureEncoder(cfg.gse_model.hidden_dim)
+        self.block_dict = nn.ModuleDict()
 
         poly_method = "sparse_" + cfg.posenc_Poly.method
-        self.block_dict = nn.ModuleDict()
+        LoopEncoder = register.node_encoder_dict["sparse_poly"]
+        ConnEncoder = register.edge_encoder_dict["sparse_poly"]
         for lidx in range(cfg.gse_model.messaging.num_blocks):
             loop_encoder = LoopEncoder(poly_method, cfg.posenc_Poly.emb_dim, cfg.gse_model.hidden_dim)
             conn_encoder = ConnEncoder(poly_method, cfg.posenc_Poly.emb_dim, cfg.gse_model.hidden_dim)
@@ -88,6 +88,13 @@ class GseModel(torch.nn.Module):
             self.block_dict[f"{lidx}_loop_enc"] = loop_encoder
             self.block_dict[f"{lidx}_conn_enc"] = conn_encoder
             self.block_dict[f"{lidx}_messaging"] = messaging_block
+
+        if cfg.gse_model.messaging.num_blocks == 0:
+            assert cfg.gse_model.full.enable
+            loop_encoder = LoopEncoder(poly_method, cfg.posenc_Poly.emb_dim, cfg.gse_model.hidden_dim)
+            conn_encoder = ConnEncoder(poly_method, cfg.posenc_Poly.emb_dim, cfg.gse_model.hidden_dim)
+            self.block_dict["all_loop_enc"] = loop_encoder
+            self.block_dict["all_conn_enc"] = conn_encoder
 
         if cfg.gse_model.full.enable:
             repeats = cfg.gse_model.full.repeats
@@ -122,12 +129,13 @@ class GseModel(torch.nn.Module):
 
         for lidx in range(cfg.gse_model.messaging.num_blocks):
             order = self._poly_order_map[lidx]
-            if lidx < cfg.gse_model.messaging.num_blocks:
+            if lidx < cfg.gse_model.messaging.num_blocks - 1:
                 poly_sgn = all_poly_val[:, order - 1] != 0
                 poly_idx = all_poly_idx[:, poly_sgn]
                 poly_val = all_poly_val[poly_sgn, :]
             else:
                 # poly_sgn: all true
+                # assert order == all_poly_val.size(1)
                 poly_idx = all_poly_idx
                 poly_val = all_poly_val
 
@@ -150,23 +158,29 @@ class GseModel(torch.nn.Module):
 
         if cfg.gse_model.full.enable:
             full_poly = "full_" + cfg.posenc_Poly.method
-            if 'full_index' in batch:
-                full_index = batch['full_index']
-            else:
-                full_index = compute_full_index(batch.batch)
 
-            if full_index.size(1) > poly_val.size(0):
-                full_val = poly_h.new_zeros((full_index.size(1), cfg.gse_model.hidden_dim))
-                full_index, full_val = torch_sparse.coalesce(
-                    torch.cat([full_index, batch[sparse_poly + "_index"]], dim=1),
-                    torch.cat([full_val, batch[sparse_poly + "_conn"]], dim=0),
-                    batch.num_nodes, batch.num_nodes, op="add",
-                )
-                batch[full_poly + "_index"] = full_index
-                batch[full_poly + "_conn"] = full_val
-            else:
-                batch[full_poly + "_index"] = batch[sparse_poly + "_index"]
-                batch[full_poly + "_conn"] = batch[sparse_poly + "_conn"]
+            if cfg.gse_model.messaging.num_blocks == 0:
+                loop_h = self.block_dict["all_loop_enc"](all_loop_val)
+                poly_h = self.block_dict["all_conn_enc"](all_poly_val)
+                batch.x += loop_h
+
+            if cfg.gse_model.full.layer_type in {'grit'}:
+                if 'full_index' in batch:
+                    full_index = batch['full_index']
+                else:
+                    full_index = compute_full_index(batch.batch)
+                if full_index.size(1) > poly_h.size(0):
+                    full_val = poly_h.new_zeros((full_index.size(1), cfg.gse_model.hidden_dim))
+                    full_index, full_h = torch_sparse.coalesce(
+                        torch.cat([full_index, batch[sparse_poly + "_index"]], dim=1),
+                        torch.cat([full_val, batch[sparse_poly + "_conn"]], dim=0),
+                        batch.num_nodes, batch.num_nodes, op="add",
+                    )
+                    batch[full_poly + "_index"] = full_index
+                    batch[full_poly + "_conn"] = full_h
+                else:
+                    batch[full_poly + "_index"] = full_index
+                    batch[full_poly + "_conn"] = poly_h
 
             batch = self.block_dict["full"](batch)
 
--- scripts/cluster/cluster-GSE-Poly.sh
+++ scripts/cluster/cluster-GSE-Poly.sh
@@ -1,10 +1,10 @@
 
-CUDA_VISIBLE_DEVICES=0 \
-python main.py --repeat 5 --cfg configs/GSE/cluster/cluster-GSE_grit-Poly.yaml  &
-
-# wait
-
-CUDA_VISIBLE_DEVICES=1 \
+CUDA_VISIBLE_DEVICES=3 \
 python main.py --repeat 5 --cfg configs/GSE/cluster/cluster-GSE_dense-Poly.yaml  &
+
+wait
+
+CUDA_VISIBLE_DEVICES=3 \
+python main.py --repeat 5 --cfg configs/GSE/cluster/cluster-GSE_grit-Poly.yaml  &
 
 wait
