84ac88d369cbf265bfe3e2250d6956a7de1a3e2b
RongqinChen <yc07917@umac.mo>
2024-05-25 18:29:50 +0800

    update

--- scripts/peptides/fun-GSE_grit-Poly-FullOnly.sh
+++ scripts/peptides/fun-GSE_grit-Poly-FullOnly.sh
@@ -6,11 +6,11 @@ which python
 
 
 for((seed=4;seed>=0;seed--));  
-do   
+do
 
 K=12
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_grit-Poly-full.yaml seed $((seed))  \
+python main.py --cfg configs/GSE/peptides/peptides_func-GSE_grit-Poly-full.yaml seed $((seed))  \
 posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
 gse_model.attn_drop_prob 0.2 \
 gse_model.messaging.num_blocks 0 \
@@ -22,7 +22,7 @@ wait
 
 K=10
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_grit-Poly-full.yaml seed $((seed))  \
+python main.py --cfg configs/GSE/peptides/peptides_func-GSE_grit-Poly-full.yaml seed $((seed))  \
 posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
 gse_model.attn_drop_prob 0.2 \
 gse_model.messaging.num_blocks 0 \
@@ -34,7 +34,7 @@ wait
 
 K=8
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_grit-Poly-full.yaml seed $((seed))  \
+python main.py --cfg configs/GSE/peptides/peptides_func-GSE_grit-Poly-full.yaml seed $((seed))  \
 posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
 gse_model.attn_drop_prob 0.2 \
 gse_model.messaging.num_blocks 0 \
@@ -46,7 +46,7 @@ wait
 
 K=14
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_grit-Poly-full.yaml seed $((seed))  \
+python main.py --cfg configs/GSE/peptides/peptides_func-GSE_grit-Poly-full.yaml seed $((seed))  \
 posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
 gse_model.attn_drop_prob 0.2 \
 gse_model.messaging.num_blocks 0 \
