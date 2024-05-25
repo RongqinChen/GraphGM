c98e58ed4bf4fdef13bf98c21a0cd9e10256cac6
RongqinChen <yc07917@umac.mo>
2024-05-25 17:45:29 +0800

    update

--- jobs.sh
+++ jobs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name              bench_2
+#SBATCH --job-name              struct2
 #SBATCH --time                  48:00:00
 #SBATCH --cpus-per-task         6
 #SBATCH --gres                  gpu:1
 #SBATCH --mem                   200G
-#SBATCH --output=output/bench_2.out
+#SBATCH --output=output/struct2.out
 #SBATCH --partition             a100_batch
 
 your_cleanup_function()
@@ -77,13 +77,100 @@ echo ""
 
 # wait 
 
-# ====== Task: 25658 ======
-CUDA_VISIBLE_DEVICES=0 \
-python main.py --cfg configs/GSE/bench_mnist/mnist-GSE_grit-Poly.yaml name_tag M0F3 & 
+# # ====== Task: 25658 ======
+# CUDA_VISIBLE_DEVICES=0 \
+# python main.py --cfg configs/GSE/bench_mnist/mnist-GSE_grit-Poly.yaml name_tag M0F3 & 
 
-CUDA_VISIBLE_DEVICES=0 \
-python main.py --cfg configs/GSE/bench_pattern/pattern-GSE_grit-Poly.yaml M6r1Fr3 & 
+# CUDA_VISIBLE_DEVICES=0 \
+# python main.py --cfg configs/GSE/bench_pattern/pattern-GSE_grit-Poly.yaml M6r1Fr3 & 
 
-wait 
+# wait 
+
+
+# ========= Task 25877 =====================
+# K=8
+# python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_grit-full.yaml seed 4 \
+# posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+# name_tag LowMiddleK$((K))
+
+# K=16
+# python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_grit-full.yaml seed 4 \
+# posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+# name_tag LowMiddleK$((K))
+
+
+# K=12
+# python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_grit-full.yaml seed 4 \
+# posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+# name_tag LowMiddleK$((K))
+
+
+# ========= Task 25876 =====================
+
+# K=8
+# python main.py --cfg configs/GSE/peptides/peptides_func-GSE_grit-Poly-full.yaml seed 4 \
+# posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+# name_tag LowMiddleK$((K))
+
+
+# K=12
+# python main.py --cfg configs/GSE/peptides/peptides_func-GSE_grit-Poly-full.yaml seed 4 \
+# posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+# name_tag LowMiddleK$((K))
+
+
+# K=16
+# python main.py --cfg configs/GSE/peptides/peptides_func-GSE_grit-Poly-full.yaml seed 4 \
+# posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+# name_tag LowMiddleK$((K))
+
+
+
+# # ============ Task - 25879 ============
+# K=8
+# python main.py --cfg configs/GSE/peptides/peptides_func-GSE_grit-Poly-full.yaml seed 4 \
+# posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+# gse_model.attn_drop_prob 0.2 \
+# name_tag LowMiddleK$((K))
+
+
+# K=12
+# python main.py --cfg configs/GSE/peptides/peptides_func-GSE_grit-Poly-full.yaml seed 4 \
+# posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+# gse_model.attn_drop_prob 0.2 \
+# name_tag LowMiddleK$((K))
+
+
+# K=16
+# python main.py --cfg configs/GSE/peptides/peptides_func-GSE_grit-Poly-full.yaml seed 4 \
+# posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+# gse_model.attn_drop_prob 0.2 \
+# name_tag LowMiddleK$((K))
+
+
+
+# ========= Task 25880 =====================
+K=8
+python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_grit-full.yaml seed 4 \
+posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+gse_model.attn_drop_prob 0.1 \
+gse_model.drop_prob 0.0 \
+name_tag LowMiddleK$((K))
+
+K=16
+python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_grit-full.yaml seed 4 \
+posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+gse_model.attn_drop_prob 0.1 \
+gse_model.drop_prob 0.0 \
+name_tag LowMiddleK$((K))
+
+
+K=12
+python main.py --cfg configs/GSE/peptides/peptides_struct-GSE_grit-full.yaml seed 4 \
+posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+gse_model.attn_drop_prob 0.1 \
+gse_model.drop_prob 0.0 \
+name_tag LowMiddleK$((K))
+
 
 echo "finished!!!"
