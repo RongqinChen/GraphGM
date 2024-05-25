81861ffded49b51f4a01067e8fb35d207b3a1b99
RongqinChen <yc07917@umac.mo>
2024-05-25 21:21:52 +0800

    update

--- scripts/zinc/zincfull-GSE-Poly-Doubling.sh
+++ scripts/zinc/zincfull-GSE-Poly-Doubling.sh
@@ -3,7 +3,7 @@ do
 
 K=6
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --cfg configs/GSE/zinc/zinc-GSE.yaml seed $((seed))  \
+python main.py --cfg configs/GSE/zinc/zincfull-GSE.yaml seed $((seed))  \
 posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
 gse_model.messaging.num_blocks 4 \
 gse_model.messaging.repeats 2 \
@@ -13,7 +13,7 @@ name_tag LowMiddleK$((K)) &
 
 K=8
 CUDA_VISIBLE_DEVICES=1 \
-python main.py --cfg configs/GSE/zinc/zinc-GSE.yaml seed $((seed))  \
+python main.py --cfg configs/GSE/zinc/zincfull-GSE.yaml seed $((seed))  \
 posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
 gse_model.messaging.num_blocks 4 \
 gse_model.messaging.repeats 2 \
@@ -24,7 +24,7 @@ name_tag LowMiddleK$((K)) &
 
 K=10
 CUDA_VISIBLE_DEVICES=2 \
-python main.py --cfg configs/GSE/zinc/zinc-GSE.yaml seed $((seed))  \
+python main.py --cfg configs/GSE/zinc/zincfull-GSE.yaml seed $((seed))  \
 posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
 gse_model.hidden_dim 56 \
 gse_model.messaging.num_blocks 5 \
@@ -34,7 +34,7 @@ name_tag LowMiddleK$((K)) &
 
 K=12
 CUDA_VISIBLE_DEVICES=3 \
-python main.py --cfg configs/GSE/zinc/zinc-GSE.yaml seed $((seed))  \
+python main.py --cfg configs/GSE/zinc/zincfull-GSE.yaml seed $((seed))  \
 posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
 gse_model.hidden_dim 56 \
 gse_model.messaging.num_blocks 5 \
