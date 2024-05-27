bdb9f4c84cde826fc05e634e6a69eee346bd2ddd
RongqinChen <yc07917@umac.mo>
2024-05-27 17:10:26 +0800

    update

--- scripts/zinc/zinc-GSE-Poly-FullOnly.sh
+++ scripts/zinc/zinc-GSE-Poly-FullOnly.sh
@@ -1,61 +1,48 @@
+for((seed=4;seed>=0;seed--));  
+do   
 
-K=8
+K=10
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GSE.yaml  \
+python main.py --cfg configs/GSE/zinc/zinc-GSE.yaml seed $((seed))  \
 posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+gse_model.attn_drop_prob 0.20 \
 gse_model.messaging.num_blocks 0 \
 gse_model.full.repeats 10 \
-name_tag LowMiddleK$((K)) &
+name_tag LowMiddleK$((K))Adp20 &
+
+# wait
+
+K=8
+CUDA_VISIBLE_DEVICES=1 \
+python main.py --cfg configs/GSE/zinc/zinc-GSE.yaml seed $((seed))  \
+posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+gse_model.attn_drop_prob 0.20 \
+gse_model.messaging.num_blocks 0 \
+gse_model.full.repeats 10 \
+name_tag LowMiddleK$((K))Adp20 &
 
 # wait
 
 K=12
 CUDA_VISIBLE_DEVICES=0 \
-python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GSE.yaml  \
+python main.py --cfg configs/GSE/zinc/zinc-GSE.yaml seed $((seed))  \
 posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+gse_model.attn_drop_prob 0.20 \
 gse_model.messaging.num_blocks 0 \
 gse_model.full.repeats 10 \
-name_tag LowMiddleK$((K)) &
+name_tag LowMiddleK$((K))Adp20 &
 
-wait
+# wait
 
 K=6
-CUDA_VISIBLE_DEVICES=0 \
-python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GSE.yaml  \
+CUDA_VISIBLE_DEVICES=1 \
+python main.py --cfg configs/GSE/zinc/zinc-GSE.yaml seed $((seed))  \
 posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
+gse_model.attn_drop_prob 0.20 \
 gse_model.messaging.num_blocks 0 \
 gse_model.full.repeats 10 \
-name_tag LowMiddleK$((K)) &
-
-# wait
-
-K=16
-CUDA_VISIBLE_DEVICES=0 \
-python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GSE.yaml  \
-posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
-gse_model.messaging.num_blocks 0 \
-gse_model.full.repeats 10 \
-name_tag LowMiddleK$((K)) &
+name_tag LowMiddleK$((K))Adp20 &
 
 wait
 
-
-K=18
-CUDA_VISIBLE_DEVICES=0 \
-python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GSE.yaml  \
-posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
-gse_model.messaging.num_blocks 0 \
-gse_model.full.repeats 10 \
-name_tag LowMiddleK$((K)) &
-
-# wait
-
-K=20
-CUDA_VISIBLE_DEVICES=0 \
-python main.py --repeat 5 --cfg configs/GSE/zinc/zinc-GSE.yaml  \
-posenc_Poly.method low_middle_pass posenc_Poly.order $((K)) posenc_Poly.emb_dim $((K))  \
-gse_model.messaging.num_blocks 0 \
-gse_model.full.repeats 10 \
-name_tag LowMiddleK$((K)) &
-
-wait
+done  
