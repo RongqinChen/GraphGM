eb5570f256fd093bd7db2941255c2b2e555ee5aa
RongqinChen <yc07917@umac.mo>
2024-05-29 17:57:58 +0000

    update

---
+++ scripts/peptides/func-MBP_mixedbern-GRIT-full.sh
@@ -0,0 +1,35 @@
+
+which python
+
+source /home/yc07917/mambaforge/bin/activate gnn210
+which python
+
+
+for((seed=4;seed>=0;seed--));  
+do   
+
+K=8
+CUDA_VISIBLE_DEVICES=0 \
+python main.py --cfg configs/MBP/peptides/peptides_func-MBP_mixedbern-GRIT-full.yamlconfigs/MBP/peptides/peptides_func-MBP_mixedbern-GRIT-full.yaml seed $((seed)) &
+
+wait
+
+K=16
+CUDA_VISIBLE_DEVICES=0 \
+python main.py --cfg configs/MBP/peptides/peptides_func-MBP_mixedbern-GRIT-full.yamlconfigs/MBP/peptides/peptides_func-MBP_mixedbern-GRIT-full.yaml seed $((seed)) &
+
+wait
+
+K=10
+CUDA_VISIBLE_DEVICES=0 \
+python main.py --cfg configs/MBP/peptides/peptides_func-MBP_mixedbern-GRIT-full.yamlconfigs/MBP/peptides/peptides_func-MBP_mixedbern-GRIT-full.yaml seed $((seed)) &
+
+wait
+
+K=12
+CUDA_VISIBLE_DEVICES=0 \
+python main.py --cfg configs/MBP/peptides/peptides_func-MBP_mixedbern-GRIT-full.yamlconfigs/MBP/peptides/peptides_func-MBP_mixedbern-GRIT-full.yaml seed $((seed)) &
+
+wait
+
+done  
