00dfc933a861f01f564b4fe3dc3cf9c0588b4125
RongqinChen <yc07917@umac.mo>
2024-06-03 15:20:24 +0800

    update

--- scripts/bench_pattern/pattern-MBP_mixedbern-GRIT-full.sh
+++ scripts/bench_pattern/pattern-MBP_mixedbern-GRIT-full.sh
@@ -2,7 +2,7 @@ for((seed=4;seed>=0;seed--));
 do   
 
 K=4 
-CUDA_VISIBLE_DEVICES=0 \
+CUDA_VISIBLE_DEVICES=3 \
 python main.py --cfg configs/MBP/bench_pattern/pattern-MBP_mixedbern-GRIT-full.yaml seed $((seed))  \
 posenc_Poly.power $((K)) \
 name_tag K$((K)) &
@@ -10,7 +10,7 @@ name_tag K$((K)) &
 wait
 
 K=6 
-CUDA_VISIBLE_DEVICES=0 \
+CUDA_VISIBLE_DEVICES=3 \
 python main.py --cfg configs/MBP/bench_pattern/pattern-MBP_mixedbern-GRIT-full.yaml seed $((seed))  \
 posenc_Poly.power $((K)) \
 name_tag K$((K)) &
@@ -18,7 +18,7 @@ name_tag K$((K)) &
 wait
 
 K=8 
-CUDA_VISIBLE_DEVICES=0 \
+CUDA_VISIBLE_DEVICES=3 \
 python main.py --cfg configs/MBP/bench_pattern/pattern-MBP_mixedbern-GRIT-full.yaml seed $((seed))  \
 posenc_Poly.power $((K)) \
 name_tag K$((K)) &
