fce0b78d09685c31fad8c4603d497c596d2bbb00
RongqinChen <yc07917@umac.mo>
2024-06-03 20:40:17 +0800

    update

---
+++ scripts/peptides/func-MBP_mixedbern-GRIT-sparse.sh
@@ -0,0 +1,31 @@
+for((seed=4;seed>=0;seed--));  
+do
+
+
+K=8
+CUDA_VISIBLE_DEVICES=2 \
+python main.py --cfg configs/MBP/peptides/peptides_func-MBP_mixedbern-GRIT-sparse.yaml \
+posenc_Poly.power $((K)) mbp_model.drop_prob 0.0 \
+mbp_model.hidden_dim 80 mbp_model.messaging.num_blocks 4 \
+seed $((seed)) name_tag K$((K))DP00 &
+
+# wait
+
+K=8
+CUDA_VISIBLE_DEVICES=3 \
+python main.py --cfg configs/MBP/peptides/peptides_func-MBP_mixedbern-GRIT-sparse.yaml \
+posenc_Poly.power $((K)) mbp_model.drop_prob 0.05 \
+mbp_model.hidden_dim 80 mbp_model.messaging.num_blocks 4 \
+seed $((seed)) name_tag K$((K))DP05 &
+
+wait
+
+# K=12
+# CUDA_VISIBLE_DEVICES=0 \
+# python main.py --cfg configs/MBP/peptides/peptides_func-MBP_mixedbern-GRIT-sparse.yaml \
+# posenc_Poly.power $((K)) mbp_model.hidden_dim 80 mbp_model.messaging.num_blocks 5 \
+# seed $((seed)) name_tag K$((K)) &
+
+# wait
+
+done  
