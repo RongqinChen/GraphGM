7a46e015b7bfa2e1eb68be7d58d4af74db04ba14
RongqinChen <yc07917@umac.mo>
2024-06-12 09:25:28 +0800

    update

--- scripts/counting/cycle.sh
+++ scripts/counting/cycle.sh
@@ -1,3 +1,16 @@
+for((task=0;task<4;task++));  
+do
+
+CUDA_VISIBLE_DEVICES=0 python train_count.py --config_file configs/MBP/count/count-MBP_adj_powers-GINE-full.yaml --task $((task)) --name_tag Circle$((task))  &
+
+CUDA_VISIBLE_DEVICES=1 python train_count.py --config_file configs/MBP/count/count-MBP_bern-GINE-full.yaml --task $((task)) --name_tag Circle$((task))  &
+
+CUDA_VISIBLE_DEVICES=2 python train_count.py --config_file configs/MBP/count/count-MBP-mixed_sym_bern-GINE-full.yaml --task $((task)) --name_tag Circle$((task))  &
+
+wait
+
+done
+
 for((task=0;task<=2;task++));  
 do
 
@@ -5,9 +18,7 @@ CUDA_VISIBLE_DEVICES=0 python train_count.py --config_file configs/MBP/count/cou
 
 CUDA_VISIBLE_DEVICES=1 python train_count.py --config_file configs/MBP/count/count-MBP_bern-GINE-full.yaml --task $((task)) --name_tag Circle$((task))  &
 
-CUDA_VISIBLE_DEVICES=2 python train_count.py --config_file configs/MBP/count/count-MBP-mixed_low_bern-GINE-full.yaml --task $((task)) --name_tag Circle$((task))  &
-
-CUDA_VISIBLE_DEVICES=3 python train_count.py --config_file configs/MBP/count/count-MBP-mixed_sym_bern-GINE-full.yaml --task $((task)) --name_tag Circle$((task))  &
+CUDA_VISIBLE_DEVICES=2 python train_count.py --config_file configs/MBP/count/count-MBP-mixed_sym_bern-GINE-full.yaml --task $((task)) --name_tag Circle$((task))  &
 
 wait
 
