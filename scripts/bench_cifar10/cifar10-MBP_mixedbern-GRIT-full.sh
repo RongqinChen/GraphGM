dace9564db0fa66d74c45c6e8407b85e19b0f727
RongqinChen <yc07917@umac.mo>
2024-06-12 15:10:31 +0800

    update

--- scripts/bench_cifar10/cifar10-MBP_mixedbern-GRIT-full.sh
+++ scripts/bench_cifar10/cifar10-MBP_mixedbern-GRIT-full.sh
@@ -1,9 +1,9 @@
 for((seed=4;seed>=0;seed--));  
 do   
 
-K=18 
+K=18
 CUDA_VISIBLE_DEVICES=0 python main.py \
---cfg configs/MBP/bench_cifar10/cifar10-MBP_mixed_low_bern-GRIT-full.yaml \
+--cfg configs/MBP/bench_cifar10/cifar10-MBP_mixed_low_bern-CATTN-full.yaml \
 seed $((seed))  \
 posenc_Poly.power $((K)) \
 name_tag K$((K))
