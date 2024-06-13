ff3761c7fb63339b0eb4bc0ddd71a9366f2c1633
RongqinChen <yc07917@umac.mo>
2024-06-13 20:40:20 +0800

    update

--- scripts/brec/cattn.sh
+++ scripts/brec/cattn.sh
@@ -1,6 +1,12 @@
+# export CUDA_VISIBLE_DEVICES=1
+CUDA_VISIBLE_DEVICES=0 python train_BREC.py --config_file configs/MBP/brec/brec-MBP_adj_powers-CATTN-full.yaml &
+# wait
 
-python train_BREC.py --config_file configs/MBP/brec/brec-MBP_bern-CATTN-full.yaml &
+# python train_BREC.py --config_file configs/MBP/brec/brec-MBP_bern-CATTN-full.yaml &
+# wait
+
+CUDA_VISIBLE_DEVICES=1 python train_BREC.py --config_file configs/MBP/brec/brec-MBP_mixed_sym_bern-CATTN-full.yaml &
 wait
 
-python train_BREC.py --config_file configs/MBP/brec/brec-MBP_mixed_sym_bern-CATTN-full.yaml &
-wait
+# python train_BREC.py --config_file configs/MBP/brec/brec-MBP_mixed_low_bern-CATTN-full.yaml &
+# wait
